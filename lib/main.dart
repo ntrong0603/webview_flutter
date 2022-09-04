e7303eadd90a5d493d49f159eb0c9b64d8dd1685
DESKTOP-PB9LET4\ntrong <ntrong0603.dgt@gmail.com>
2022-09-04 21:54:31 +0700

    first

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';  // Add this import back
+
+import 'src/resource/menu.dart';
+import 'src/resource/navigation_controls.dart';
+import 'src/resource/web_view_stack.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: WebViewApp(),
+    ),
+  );
+}
+
+class WebViewApp extends StatefulWidget {
+  const WebViewApp({super.key});
+
+  @override
+  State<WebViewApp> createState() => _WebViewAppState();
+}
+
+class _WebViewAppState extends State<WebViewApp> {
+  final controller = Completer<WebViewController>();    // Instantiate the controller
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter WebView'),
+        actions: [
+          NavigationControls(controller: controller),
+          Menu(controller: controller),
+        ],
+      ),
+      body: WebViewStack(controller: controller),
+    );
+  }
+}
